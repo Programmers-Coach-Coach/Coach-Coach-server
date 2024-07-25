1eefef8e1fcfd3bfc983a2ae4e6b859411827c8b
ckwogus37 <ckwogus37@gmail.com>
2024-07-25 16:15:49 +0900

    initial commit

---
+++ src/main/java/site/coach_coach/Coach_Coach_server/CoachCoachServerApplication.java
@@ -0,0 +1,13 @@
+package site.coach_coach.Coach_Coach_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachCoachServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachCoachServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/coach_coach/Coach_Coach_server/CoachCoachServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.coach_coach.Coach_Coach_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachCoachServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
