52e3ee6038f73b08aa5a9547f08d3302d3b39020
Hanbi <adray0625@naver.com>
2024-07-28 18:42:13 +0900

    feat(user) : user controller, service 틀 작성

---
+++ src/main/java/site/coach_coach/Coach_Coach_server/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package site.coach_coach.Coach_Coach_server.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class UserController {
+	// @PostMapping("/auth/signup")
+	// public ResponseEntity<UserDto> signup(@RequestBody @Valid SignupDto signupDto) {
+	//
+	// }
+}
---
+++ src/main/java/site/coach_coach/Coach_Coach_server/user/service/UserService.java
@@ -0,0 +1,7 @@
+package site.coach_coach.Coach_Coach_server.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
