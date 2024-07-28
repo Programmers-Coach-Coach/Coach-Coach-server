fc792e0409b31a57d76d08dd05a2fcf738dbd9b6
ckwogus37 <ckwogus37@gmail.com>
2024-07-28 20:34:06 +0900

    chore(ci) : 임시 루틴 추가 기능 제거 및 CI 적용

--- src/main/java/site/coach_coach/Coach_Coach_server/routine/domain/Routine.java
+++ src/main/java/site/coach_coach/Coach_Coach_server/routine/domain/Routine.java
@@ -1,6 +1,10 @@
 package site.coach_coach.Coach_Coach_server.routine.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,27 +16,27 @@ import lombok.Setter;
 @Entity
 public class Routine {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column(name="user_id")
-    private Integer userId;
+	@Column(name = "user_id")
+	private Integer userId;
 
-    @Column(name="coach_id")
-    private Integer coachId;
+	@Column(name = "coach_id")
+	private Integer coachId;
 
-    @Column(name="sports_id")
-    private Integer sportsId;
+	@Column(name = "sports_id")
+	private Integer sportsId;
 
-    @Column(name="routine_Name")
-    private String routineName;
+	@Column(name = "routine_Name")
+	private String routineName;
 
-    @Builder
-    public Routine(Integer userId, Integer coachId, Integer sportsId, String routineName){
-        this.userId = userId;
-        this.coachId = coachId;
-        this.sportsId = sportsId;
-        this.routineName = routineName;
-    }
+	@Builder
+	public Routine(Integer userId, Integer coachId, Integer sportsId, String routineName) {
+		this.userId = userId;
+		this.coachId = coachId;
+		this.sportsId = sportsId;
+		this.routineName = routineName;
+	}
 }
