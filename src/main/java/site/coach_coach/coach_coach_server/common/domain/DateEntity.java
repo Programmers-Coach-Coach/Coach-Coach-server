3cecf91faefe73a4650cc03d974aa444ebca95d1
Hanbi <adray0625@naver.com>
2024-08-02 23:16:45 +0900

    feat : date entity updatedAt 추가, securityconfig 수정

--- src/main/java/site/coach_coach/coach_coach_server/common/domain/DateEntity.java
+++ src/main/java/site/coach_coach/coach_coach_server/common/domain/DateEntity.java
@@ -2,25 +2,18 @@ package site.coach_coach.coach_coach_server.common.domain;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 public class DateEntity {
-	@Column(nullable = false)
+	@CreationTimestamp
 	private LocalDateTime createdAt;
 
-	@PrePersist
-	public void onPrePersist() {
-		createdAt = LocalDateTime.now();
-	}
-
-	@PreUpdate
-	public void onPreUpdate() {
-		createdAt = LocalDateTime.now();
-	}
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
 }
