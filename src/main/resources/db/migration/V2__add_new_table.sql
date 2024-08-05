53f3828feee2332bd77d14ee454724c23516aeed
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-08-05 23:13:58 +0900

    feat : refresh_tokens table 추가 (#51)

---
+++ src/main/resources/db/migration/V2__add_new_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `refresh_tokens`
+(
+    `refresh_token_id` bigint                           NOT NULL,
+    `user_id`          bigint                           NOT NULL,
+    `expire_date`      timestamp                        NOT NULL,
+    `refresh_token`    varchar(200) COLLATE utf8mb4_bin NOT NULL,
+    `created_at`       timestamp                        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`refresh_token_id`),
+    KEY `fk_refresh_tokens_user_id_idx` (`user_id`),
+    CONSTRAINT `fk_refresh_tokens_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_bin
