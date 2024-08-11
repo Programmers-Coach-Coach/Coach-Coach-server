cf37167956c84030648433a79566727e2a0b1db5
jeehdev <jeehdev@gmail.com>
2024-08-11 17:46:41 +0900

    refactor : 중복 코드 제거

--- src/main/resources/db/migration/V4__modify_column_settings.sql
+++ src/main/resources/db/migration/V4__modify_column_settings.sql
@@ -2,8 +2,6 @@ ALTER TABLE `coachcoach`.`users`
     ADD COLUMN `local_address_detail` VARCHAR(100) NULL DEFAULT NULL AFTER `local_address`,
     CHANGE COLUMN `profile_image_url` `profile_image_url` VARCHAR(500) NULL DEFAULT NULL ,
     CHANGE COLUMN `local_info` `local_address` VARCHAR(100) NULL DEFAULT NULL ;
-ALTER TABLE `coachcoach`.`sports`
-    CHANGE COLUMN `sport_image_url` `sport_image_url` VARCHAR(500) NOT NULL ;
 
 ALTER TABLE `coachcoach`.`sports`
     CHANGE COLUMN `sport_image_url` `sport_image_url` VARCHAR(500) NOT NULL ;
