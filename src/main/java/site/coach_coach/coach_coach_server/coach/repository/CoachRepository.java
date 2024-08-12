200417079a21d33bdc8d8be3561bbfa7a90a4fcd
jeeehdev <154660538+jeeehdev@users.noreply.github.com>
2024-08-12 19:00:40 +0900

    refactor : Update CoachRepository.java

--- src/main/java/site/coach_coach/coach_coach_server/coach/repository/CoachRepository.java
+++ src/main/java/site/coach_coach/coach_coach_server/coach/repository/CoachRepository.java
@@ -1,11 +1,11 @@
 package site.coach_coach.coach_coach_server.coach.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import site.coach_coach.coach_coach_server.coach.domain.Coach;
-
-import java.util.*;
 
 @Repository
 public interface CoachRepository extends JpaRepository<Coach, Long> {
