d1214b19ebb7076c7650fcb6a8f4f406e227bf70
Hanbi <adray0625@naver.com>
2024-08-12 18:35:23 +0900

    feat(user) : params validation 적용

--- src/main/java/site/coach_coach/coach_coach_server/user/controller/UserController.java
+++ src/main/java/site/coach_coach/coach_coach_server/user/controller/UserController.java
@@ -16,6 +16,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import site.coach_coach.coach_coach_server.auth.jwt.TokenProvider;
 import site.coach_coach.coach_coach_server.auth.jwt.dto.TokenDto;
@@ -26,6 +27,7 @@ import site.coach_coach.coach_coach_server.user.domain.User;
 import site.coach_coach.coach_coach_server.user.dto.LoginRequest;
 import site.coach_coach.coach_coach_server.user.dto.SignUpRequest;
 import site.coach_coach.coach_coach_server.user.service.UserService;
+import site.coach_coach.coach_coach_server.user.validation.Nickname;
 
 @RestController
 @RequestMapping("/api")
@@ -70,13 +72,13 @@ public class UserController {
 	}
 
 	@GetMapping("/v1/auth/check-nickname")
-	public ResponseEntity<Void> checkNickname(@RequestParam("nickname") String nickname) {
+	public ResponseEntity<Void> checkNickname(@RequestParam("nickname") @Nickname String nickname) {
 		userService.checkNicknameDuplicate(nickname);
 		return ResponseEntity.noContent().build();
 	}
 
 	@GetMapping("/v1/auth/check-email")
-	public ResponseEntity<Void> checkEmail(@RequestParam("email") String email) {
+	public ResponseEntity<Void> checkEmail(@RequestParam("email") @Email String email) {
 		userService.checkEmailDuplicate(email);
 		return ResponseEntity.noContent().build();
 	}
