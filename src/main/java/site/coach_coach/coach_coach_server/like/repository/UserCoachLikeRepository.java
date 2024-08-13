8b71a34efd6b71a0b8b93aaa8373d2418e2b406e
jeehdev <jeehdev@gmail.com>
2024-08-13 17:50:58 +0900

    refactor : checkStyle 에러 해결

--- src/main/java/site/coach_coach/coach_coach_server/like/repository/UserCoachLikeRepository.java
+++ src/main/java/site/coach_coach/coach_coach_server/like/repository/UserCoachLikeRepository.java
@@ -16,6 +16,7 @@ public interface UserCoachLikeRepository extends JpaRepository<UserCoachLike, Lo
 
 	boolean existsByUser_UserIdAndCoach_CoachId(Long userId, Long coachId);
 
-	@Query("SELECT u.coach FROM UserCoachLike u WHERE u.createdAt >= :startDate GROUP BY u.coach ORDER BY COUNT(u) DESC")
+	@Query("SELECT u.coach FROM UserCoachLike u " +
+		"WHERE u.createdAt >= :startDate GROUP BY u.coach ORDER BY COUNT(u) DESC")
 	List<Coach> findTopCoachesByLikesSince(@Param("startDate") LocalDateTime startDate, Pageable pageable);
 }
