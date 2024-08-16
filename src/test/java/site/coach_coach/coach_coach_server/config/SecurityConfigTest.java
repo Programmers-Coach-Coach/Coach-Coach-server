9919bad9c2f0e01a00c965e88a132e35d1833855
ckwogus37 <ckwogus37@gmail.com>
2024-08-16 16:14:06 +0900

    refactor(ci) : H2 dependency 추가

--- src/test/java/site/coach_coach/coach_coach_server/config/SecurityConfigTest.java
+++ src/test/java/site/coach_coach/coach_coach_server/config/SecurityConfigTest.java
@@ -5,14 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Import(SecurityConfig.class)
 public class SecurityConfigTest {
 
 	@Autowired
