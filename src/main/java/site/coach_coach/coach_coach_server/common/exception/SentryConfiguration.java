f464282e1549e9dd23d51f8f45f823dd1d81735e
jeehdev <jeehdev@gmail.com>
2024-08-17 02:01:01 +0900

    feature : sentry 설정

---
+++ src/main/java/site/coach_coach/coach_coach_server/common/exception/SentryConfiguration.java
@@ -0,0 +1,10 @@
+package site.coach_coach.coach_coach_server.common.exception;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.sentry.spring.jakarta.EnableSentry;
+
+@EnableSentry
+@Configuration
+class SentryConfiguration {
+}
