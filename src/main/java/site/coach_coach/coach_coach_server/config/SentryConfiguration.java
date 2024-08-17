bbf1abb9285b4756d6c6c25352e9bbf64a529929
jeehdev <jeehdev@gmail.com>
2024-08-17 17:17:28 +0900

    refactor : 디렉토리 위치 변경

--- src/main/java/site/coach_coach/coach_coach_server/config/SentryConfiguration.java
+++ src/main/java/site/coach_coach/coach_coach_server/config/SentryConfiguration.java
@@ -1,10 +1,10 @@
-package site.coach_coach.coach_coach_server.common.exception;
+package site.coach_coach.coach_coach_server.config;
 
 import org.springframework.context.annotation.Configuration;
 
 import io.sentry.spring.jakarta.EnableSentry;
 
-@EnableSentry
+@EnableSentry(dsn = "${sentry.dsn}")
 @Configuration
 class SentryConfiguration {
 }
