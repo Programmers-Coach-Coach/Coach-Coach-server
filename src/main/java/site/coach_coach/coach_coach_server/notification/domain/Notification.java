8d58d3e34c534e2c93e9fa17f66e961d3e17ef50
Hanbi <adray0625@naver.com>
2024-08-19 17:26:42 +0900

    feat(notification) : 알림 domain 구현

---
+++ src/main/java/site/coach_coach/coach_coach_server/notification/domain/Notification.java
@@ -0,0 +1,58 @@
+package site.coach_coach.coach_coach_server.notification.domain;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.coach_coach.coach_coach_server.common.domain.RelationFunctionEnum;
+import site.coach_coach.coach_coach_server.user.domain.User;
+
+@Table(name = "notifications")
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "notification_id")
+	private Long notificationId;
+
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@NotBlank
+	@Column(name = "message")
+	@Size(max = 100)
+	private String message;
+
+	@NotNull
+	@Column(name = "relation_function")
+	@Enumerated(EnumType.STRING)
+	private RelationFunctionEnum relationFunction;
+
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+}
