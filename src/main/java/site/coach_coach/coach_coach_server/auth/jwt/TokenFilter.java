10350a5e948cf6bdf6c7a8bae0487a7f13b1d486
Hanbi <adray0625@naver.com>
2024-08-20 11:22:11 +0900

    refactor : token 예외 처리 수정

--- src/main/java/site/coach_coach/coach_coach_server/auth/jwt/TokenFilter.java
+++ src/main/java/site/coach_coach/coach_coach_server/auth/jwt/TokenFilter.java
@@ -25,15 +25,12 @@ public class TokenFilter extends OncePerRequestFilter {
 		throws ServletException, IOException {
 		response.setCharacterEncoding("utf-8");
 
-		String requestUri = request.getRequestURI();
-		if (requestUri.equals("/api/v1/auth/login") || requestUri.equals("/api/v1/auth/signup") || requestUri.equals(
-			"/api/v1/test") || requestUri.equals("/api/v1/auth/check-email") || requestUri.equals(
-			"/api/v1/auth/check-nickname")) {
+		String accessToken = tokenProvider.getCookieValue(request, ACCESS_TOKEN);
+		
+		if (accessToken == null) {
 			filterChain.doFilter(request, response);
 			return;
 		}
-
-		String accessToken = tokenProvider.getCookieValue(request, ACCESS_TOKEN);
 
 		if (tokenProvider.validateAccessToken(accessToken)) {
 			Authentication authentication = tokenProvider.getAuthentication(accessToken);
--- src/main/java/site/coach_coach/coach_coach_server/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/site/coach_coach/coach_coach_server/config/CustomAuthenticationEntryPoint.java
@@ -10,9 +10,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import site.coach_coach.coach_coach_server.common.constants.ErrorMessage;
 import site.coach_coach.coach_coach_server.common.response.ErrorResponse;
 
+@Slf4j
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	private final ObjectMapper objectMapper = new ObjectMapper();
@@ -20,6 +22,11 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException authException) throws IOException {
+
+		log.error("Unauthorized request - Method: {}, URI: {}, Error: {}",
+			request.getMethod(),
+			request.getRequestURI(),
+			authException.getMessage());
 
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		response.setContentType("application/json");
