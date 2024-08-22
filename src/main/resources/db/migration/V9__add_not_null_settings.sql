ee0ebd66a9e4f97211d3f288b3aa5d46119e1fbd
Hanbi <adray0625@naver.com>
2024-08-22 21:15:39 +0900

    chore : db sql 추가 설정

---
+++ src/main/resources/db/migration/V9__add_not_null_settings.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `coachcoach`.`record_data`
+    CHANGE COLUMN `record_date` `record_date` DATE NOT NULL ;
+
+ALTER TABLE `coachcoach`.`completed_categories`
+    CHANGE COLUMN `record_date` `record_date` DATE NOT NULL ;
