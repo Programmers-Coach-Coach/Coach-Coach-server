1d91ad5c6b48faacfbb358df24c7099eeebe2c19
ckwogus37 <69067860+ckwogus37@users.noreply.github.com>
2024-08-24 13:28:45 +0900

    refactor : 루틴 관련 항목 삭제 검증 로직 리팩토링 (#207)
    * refactor : 객체그래프 속성을 적용하여 검증 로직 리팩토링
    
    * refactor : 불필요한 인자값 제거
    
    * refactor : 삭제 API의 URL 수정

--- src/main/java/site/coach_coach/coach_coach_server/action/service/ActionService.java
+++ src/main/java/site/coach_coach/coach_coach_server/action/service/ActionService.java
@@ -24,7 +24,7 @@ public class ActionService {
 	@Transactional
 	public Long createAction(Long routineId, Long categoryId, Long userIdByJwt,
 		CreateActionRequest createActionRequest) {
-		routineService.validateBeforeModifyRoutineDetail(routineId, userIdByJwt);
+		routineService.validateIsMyRoutine(routineId, userIdByJwt);
 		Category category = categoryRepository.findByCategoryIdAndRoutine_RoutineId(categoryId, routineId)
 			.orElseThrow(() -> new NotFoundCategoryException(ErrorMessage.NOT_FOUND_CATEGORY));
 
@@ -34,15 +34,12 @@ public class ActionService {
 	}
 
 	@Transactional
-	public void deleteAction(Long routineId, Long categoryId, Long actionId, Long userIdByJwt) {
-		routineService.validateBeforeModifyRoutineDetail(routineId, userIdByJwt);
-		categoryRepository.findByCategoryIdAndRoutine_RoutineId(categoryId, routineId)
-			.orElseThrow(() -> new NotFoundCategoryException(ErrorMessage.NOT_FOUND_CATEGORY));
+	public void deleteAction(Long actionId, Long userIdByJwt) {
+		Action action = actionRepository.findById(actionId)
+			.orElseThrow(() -> new NotFoundActionException(ErrorMessage.NOT_FOUND_ACTION));
 
-		if (actionRepository.existsById(actionId)) {
-			actionRepository.deleteById(actionId);
-		} else {
-			throw new NotFoundActionException(ErrorMessage.NOT_FOUND_ACTION);
-		}
+		routineService.validateIsMyRoutine(action.getCategory().getRoutine().getRoutineId(), userIdByJwt);
+
+		actionRepository.deleteById(actionId);
 	}
 }
