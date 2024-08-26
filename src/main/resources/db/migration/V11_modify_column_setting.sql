7b7b5c0f105714e94613e577fb317866c2462d0c
jeehdev <jeehdev@gmail.com>
2024-08-26 13:58:51 +0900

    chore : reviews 테이블 설정 변경

--- src/main/resources/db/migration/V11_modify_column_setting.sql
+++ src/main/resources/db/migration/V11_modify_column_setting.sql
@@ -3,3 +3,12 @@ ALTER TABLE `coachcoach`.`completed_categories`
 
 ALTER TABLE `coachcoach`.`user_coach_matching`
     CHANGE COLUMN `is_matching` `is_matching` TINYINT NOT NULL DEFAULT 0;
+
+ALTER TABLE `coachcoach`.`reviews`
+    DROP FOREIGN KEY `fk_reviews_user_id`;
+ALTER TABLE `coachcoach`.`reviews`
+    CHANGE COLUMN `user_id` `user_id` BIGINT NOT NULL;
+ALTER TABLE `coachcoach`.`reviews`
+    ADD CONSTRAINT `fk_reviews_user_id`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `coachcoach`.`users` (`user_id`);
