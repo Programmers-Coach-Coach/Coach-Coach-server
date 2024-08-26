aa2ae2e908de0b6d179bdfeda506f21ac08fd3bb
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-08-26 19:12:28 +0900

    refactor(record) : response에 record Id 추가 (#250)

--- src/main/java/site/coach_coach/coach_coach_server/userrecord/dto/UserRecordDetailResponse.java
+++ src/main/java/site/coach_coach/coach_coach_server/userrecord/dto/UserRecordDetailResponse.java
@@ -3,6 +3,7 @@ package site.coach_coach.coach_coach_server.userrecord.dto;
 import java.util.List;
 
 public record UserRecordDetailResponse(
+	Long recordId,
 	Double weight,
 	Double skeletalMuscle,
 	Double fatPercentage,
