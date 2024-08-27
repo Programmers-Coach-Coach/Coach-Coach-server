02afcc1bf5fe540bc9fa3f8d3a7292db81e56d09
Hanbi <adray0625@naver.com>
2024-08-27 14:49:08 +0900

    refactor(sql) : sql 코드 수정

--- src/main/resources/db/migration/V13__modify_column_setting.sql
+++ src/main/resources/db/migration/V13__modify_column_setting.sql
@@ -1,14 +1,18 @@
-ALTER TABLE `coachcoach`.`users`
-    CHANGE COLUMN `is_coach` `is_coach` TINYINT NOT NULL DEFAULT 0;
+UPDATE `coachcoach`.`users`
+SET `is_coach` = 0
+WHERE `is_coach` IS NULL;
 
 ALTER TABLE `coachcoach`.`users`
-    CHANGE COLUMN `introduction` `introduction` VARCHAR(1000) NULL DEFAULT NULL;
+    MODIFY COLUMN `is_coach` TINYINT NOT NULL DEFAULT 0;
+
+ALTER TABLE `coachcoach`.`users`
+    MODIFY COLUMN `introduction` VARCHAR(1000) NULL DEFAULT NULL;
 
 ALTER TABLE `coachcoach`.`reviews`
-    CHANGE COLUMN `contents` `contents` VARCHAR(1000) NOT NULL;
+    MODIFY COLUMN `contents` VARCHAR(1000) NOT NULL;
 
 ALTER TABLE `coachcoach`.`coaches`
-    CHANGE COLUMN `coach_introduction` `coach_introduction` VARCHAR(1000) NOT NULL;
+    MODIFY COLUMN `coach_introduction` VARCHAR(1000) NOT NULL;
 
-ALTER TABLE notifications
-    MODIFY COLUMN relation_function ENUM ('ask', 'like', 'review', 'match');
+ALTER TABLE `coachcoach`.`notifications`
+    MODIFY COLUMN `relation_function` ENUM ('ask', 'like', 'review', 'match') NOT NULL;
