37b8a981130e6d0dc7cd3a6b4e6b2d5412b450e3
jeehdev <jeehdev@gmail.com>
2024-08-27 14:22:38 +0900

    refactor : Change TINYTEXT to Varchar

--- src/main/java/site/coach_coach/coach_coach_server/coach/dto/ReviewRequestDto.java
+++ src/main/java/site/coach_coach/coach_coach_server/coach/dto/ReviewRequestDto.java
@@ -2,10 +2,11 @@ package site.coach_coach.coach_coach_server.coach.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 
 public record ReviewRequestDto(
+	@Size(max = 1000)
 	String contents,
-
 	@Min(1) @Max(5)
 	int stars
 ) {
