8298c1b03a4ead82e8956d3da27a8c5b50ac850b
Hanbi <adray0625@naver.com>
2024-08-27 15:53:24 +0900

    refactor(sql) : local_info drop 쿼리 삭제

--- src/main/resources/db/migration/V6__modify_column_settings.sql
+++ src/main/resources/db/migration/V6__modify_column_settings.sql
@@ -1,7 +1,5 @@
 ALTER TABLE `coachcoach`.`completed_categories`
     ADD COLUMN `is_completed` TINYINT NOT NULL DEFAULT 1 AFTER `routine_category_id`;
-ALTER TABLE `coachcoach`.`users`
-    DROP COLUMN `local_info`;
 ALTER TABLE `coachcoach`.`coaching_sports`
     ADD COLUMN `updated_at` `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 ALTER TABLE `coachcoach`.`user_coach_likes`
