d0608f2be4a3449da67cbb7e22aa02747cabf017
Hanbi <adray0625@naver.com>
2024-08-27 22:13:57 +0900

    fix : review 테이블에 user_id default null 추가

--- src/main/resources/db/migration/V15__reviews_user_id_null.sql
+++ src/main/resources/db/migration/V15__reviews_user_id_null.sql
@@ -1,6 +1,8 @@
 ALTER TABLE `coachcoach`.`reviews`
 DROP FOREIGN KEY `fk_reviews_user_id`;
 ALTER TABLE `coachcoach`.`reviews`
+    CHANGE COLUMN `user_id` `user_id` BIGINT NULL;
+ALTER TABLE `coachcoach`.`reviews`
     ADD CONSTRAINT `fk_reviews_user_id`
         FOREIGN KEY (`user_id`)
             REFERENCES `coachcoach`.`users` (`user_id`)
