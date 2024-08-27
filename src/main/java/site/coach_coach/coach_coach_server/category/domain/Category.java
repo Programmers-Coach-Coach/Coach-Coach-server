98a4133011275e5aac1d863104e6dba340adcd81
ckwogus37 <69067860+ckwogus37@users.noreply.github.com>
2024-08-27 16:07:25 +0900

    refactor(category) : 카테고리 완료 메서드 및 카테고리 Entity 리팩토링 (#272)
    * refactor(category) : 카테고리 Entity 어노테이션 및 카테고리 완료 메서드 리팩토링
    
    * refactor(cateogry) : is_completed 리셋 메서드 로그 리팩토링

--- src/main/java/site/coach_coach/coach_coach_server/category/domain/Category.java
+++ src/main/java/site/coach_coach/coach_coach_server/category/domain/Category.java
@@ -21,7 +21,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import site.coach_coach.coach_coach_server.action.domain.Action;
 import site.coach_coach.coach_coach_server.common.domain.DateEntity;
 import site.coach_coach.coach_coach_server.completedcategory.domain.CompletedCategory;
@@ -30,7 +29,6 @@ import site.coach_coach.coach_coach_server.routine.domain.Routine;
 @Table(name = "routine_categories")
 @Entity
 @Getter
-@Setter
 @DynamicUpdate
 @Builder
 @NoArgsConstructor
@@ -46,6 +44,7 @@ public class Category extends DateEntity {
 	@Column(name = "category_name")
 	private String categoryName;
 
+	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "routine_id")
 	private Routine routine;
