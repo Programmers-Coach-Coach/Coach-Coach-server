03320d4af0b48d9423ada1d5d6f760b11072a912
jeehdev <jeehdev@gmail.com>
2024-08-27 16:09:11 +0900

    chore : jar version update

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_VERSION="1.0.3"
+JAR_VERSION="1.0.4"
 JAR_FILE="$PROJECT_ROOT/spring-webapp-$JAR_VERSION.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
