9a3315ae9fb33ee54941bb21e730ad9bcd0b3d27
Hanbi <adray0625@naver.com>
2024-10-22 22:35:06 +0900

    chore: chat rooms로 수정

--- src/main/resources/db/migration/V19__create_chatting_table.sql
+++ src/main/resources/db/migration/V19__create_chatting_table.sql
@@ -1,23 +1,23 @@
-CREATE TABLE `coachcoach`.`chatting_rooms`
+CREATE TABLE `coachcoach`.`chat_rooms`
 (
-    `chatting_room_id` BIGINT    NOT NULL AUTO_INCREMENT,
-    `coach_id`         BIGINT    NOT NULL,
-    `user_id`          BIGINT    NOT NULL,
-    `created_at`       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    `updated_at`       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    PRIMARY KEY (`chatting_room_id`)
+    `chat_room_id` BIGINT    NOT NULL AUTO_INCREMENT,
+    `coach_id`     BIGINT    NOT NULL,
+    `user_id`      BIGINT    NOT NULL,
+    `created_at`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `updated_at`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`chat_room_id`)
 );
 
-ALTER TABLE `coachcoach`.`chatting_rooms`
-    ADD INDEX `fk_chatting_rooms_coach_id_idx` (`coach_id` ASC) VISIBLE,
-    ADD INDEX `fk_chatting_rooms_user_id_idx` (`user_id` ASC) VISIBLE;
-ALTER TABLE `coachcoach`.`chatting_rooms`
-    ADD CONSTRAINT `fk_chatting_rooms_coach_id`
+ALTER TABLE `coachcoach`.`chat_rooms`
+    ADD INDEX `fk_chat_rooms_coach_id_idx` (`coach_id` ASC) VISIBLE,
+    ADD INDEX `fk_chat_rooms_user_id_idx` (`user_id` ASC) VISIBLE;
+ALTER TABLE `coachcoach`.`chat_rooms`
+    ADD CONSTRAINT `fk_chat_rooms_coach_id`
         FOREIGN KEY (`coach_id`)
             REFERENCES `coachcoach`.`coaches` (`coach_id`)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    ADD CONSTRAINT `fk_chatting_rooms_user_id`
+    ADD CONSTRAINT `fk_chat_rooms_user_id`
         FOREIGN KEY (`user_id`)
             REFERENCES `coachcoach`.`users` (`user_id`)
             ON DELETE NO ACTION
