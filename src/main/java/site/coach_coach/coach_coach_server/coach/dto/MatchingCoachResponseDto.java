386479c9c9badc599454b5570367d0fc47bb2a27
jeehdev <jeehdev@gmail.com>
2024-11-12 16:30:18 +0900

    refactor : response 값 수정 및 추가

--- src/main/java/site/coach_coach/coach_coach_server/coach/dto/MatchingCoachResponseDto.java
+++ src/main/java/site/coach_coach/coach_coach_server/coach/dto/MatchingCoachResponseDto.java
@@ -1,9 +1,16 @@
 package site.coach_coach.coach_coach_server.coach.dto;
 
+import java.util.List;
+
+import site.coach_coach.coach_coach_server.sport.dto.CoachingSportDto;
+
 public record MatchingCoachResponseDto(
 	Long coachId,
 	String coachName,
 	String profileImageUrl,
+	String localAddress,
+	List<CoachingSportDto> coachingSports,
+	boolean isLiked,
 	boolean isMatching
 ) {
 }
